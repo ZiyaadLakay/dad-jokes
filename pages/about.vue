106ac680b23432dde67d9b1de179ee6d98ea0d54
Ziyaad Lakay <ziyaad@melio.ai>
2022-05-12 22:09:14 +0200

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>About Dadjokes</h1>
+
+    <p>This is the about Page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'About the App',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
